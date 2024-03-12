191954ada36d7fdb7a9441c1864389744a7fa002
Rustam <kodzokoff2016@yandex.ru>
2024-03-12 16:59:02 +0300

    params by fixture

--- tests_github_main_page/conftest.py
+++ tests_github_main_page/conftest.py
@@ -1,11 +1,17 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(scope='session', autouse=True)
-def browser_manager():
-    browser.open('https://github.com')
-    browser.config.window_width = 1920
-    browser.config.window_width = 1080
+
+@pytest.fixture(params=['desktop', 'mobile'], scope='session', autouse=True)
+def browser_manager_desktop(request):
+    if request.param == 'desktop':
+        browser.config.base_url = 'https://github.com/'
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+    elif request.param == 'mobile':
+        browser.config.base_url = 'https://github.com/'
+        browser.config.window_width = 414
+        browser.config.window_height = 896
 
     yield
 
--- tests_github_main_page/test_github_main_page.py
+++ tests_github_main_page/test_github_main_page.py
@@ -1,5 +1,14 @@
+import pytest
 from selene import browser
 
-def test_guthub_main_paig():
-    browser.open('https://github.com/')
+
+@pytest.mark.parametrize('browser_manager_desktop', ['desktop'], indirect=True)
+def test_github_main_page_desktop():
+    browser.open('/')
     browser.element('.HeaderMenu-link--sign-in').click()
+
+@pytest.mark.parametrize('browser_manager_desktop', ['mobile'], indirect=True)
+def test_github_main_page_mobile():
+    browser.open('/')
+    browser.element('[class="flex-1"] [data-ga-click="(Logged out) Header, clicked Sign in, text:sign-in"]'). \
+        click()
