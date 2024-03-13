8d7093363ff7150054ac8acc6d7c6c48dff44327
Rustam <kodzokoff2016@yandex.ru>
2024-03-13 22:00:43 +0300

    add third fixture

--- tests/test_github_main_page_mark.py
+++ tests/test_github_main_page_mark.py
@@ -2,16 +2,27 @@ import pytest
 from selene import browser
 
 
-def test_github_main_page_desktop(desktop_browser):
-    if browser.config.window_width == [414, 400] and browser.config.window_height == [896, 800]:
+@pytest.fixture(params=[(1920, 1080), (1600, 900), (800, 400), (896, 414)])
+def desktop_and_mobile_browser(request):
+    browser.config.base_url = 'https://github.com'
+    [width, height] = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    return width, height
+
+
+def test_github_main_page_desktop(desktop_and_mobile_browser):
+    width, height = desktop_and_mobile_browser
+    if width >= 1600:
         browser.open('/')
         browser.element('.HeaderMenu-link--sign-in').click()
     else:
         pytest.skip(reason='Skip test because needs desktop size')
 
 
-def test_github_main_page_mobile(mobile_browser):
-    if browser.config.window_width == [1920, 1600] and browser.config.window_height == [1080, 900]:
+def test_github_main_page_mobile(desktop_and_mobile_browser):
+    width, height = desktop_and_mobile_browser
+    if width < 1600:
         browser.open('/')
         browser.element('[class="flex-1"] [data-ga-click="(Logged out) Header, clicked Sign in, text:sign-in"]'). \
             click()
